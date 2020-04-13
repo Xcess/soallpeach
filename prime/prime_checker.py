9e4be3994831789eb5d07472086c59190cba4ded
Shahriar <3321951+Xcess@users.noreply.github.com>
2020-04-13 13:01:57 +0430

    strong typing

--- prime/prime_checker.py
+++ prime/prime_checker.py
@@ -3,7 +3,7 @@ from math import sqrt; from itertools import count, islice
 
 file_name = sys.argv[1]
 
-def is_prime(n):
+def is_prime(n: int) -> bool:
     return n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
 
 with open(file_name) as input_numbers:
